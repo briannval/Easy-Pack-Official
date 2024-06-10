fd75eeecc881a2a7d98f1420d966b9627736f59c
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-06-10 18:09:48 +0700

    Minor changes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -14,18 +14,16 @@ import { FiSearch } from "react-icons/fi";
 
 export default function Products() {
   return (
-    <main>
-      <VStack align={"center"} spacing={4}>
-        <Heading>Find what you're looking for.</Heading>
-        <Center>
-          <InputGroup size={"lg"} ml={"auto"} mr={"auto"}>
-            <InputLeftElement>
-              <Icon as={FiSearch} />
-            </InputLeftElement>
-            <Input type="text" placeholder="Search for products" />
-          </InputGroup>
-        </Center>
-      </VStack>
-    </main>
+    <VStack align={"center"} spacing={4} mt={"60px"} py={10}>
+      <Heading>Find what you're looking for.</Heading>
+      <Center>
+        <InputGroup size={"lg"} ml={"auto"} mr={"auto"}>
+          <InputLeftElement>
+            <Icon as={FiSearch} />
+          </InputLeftElement>
+          <Input type="text" placeholder="Search for products" />
+        </InputGroup>
+      </Center>
+    </VStack>
   );
 }
