c105e4791c979e3a8d277e973ed8cf8050b01d4e
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-06-11 13:15:03 +0700

    Add shadow

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -89,6 +89,7 @@ export default function Contact() {
               objectFit="fill"
               mr={"auto"}
               ml={"auto"}
+              boxShadow={"dark-lg"}
             />
           </Box>
         </Box>
