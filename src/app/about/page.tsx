dfa057f18793bf72686b12189e9ca61f5d562244
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-06-12 14:07:01 +0700

    Minor button for CTA

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,13 @@
 import Faq from "@/components/faq";
 import Mission from "@/components/mission";
 import Qualities from "@/components/qualities";
-import { Container, VStack } from "@chakra-ui/react";
+import { Button, Container, Icon, VStack } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { FaUtensils } from "react-icons/fa";
 
 export default function About() {
+  const router = useRouter();
+
   return (
     <main>
       <Container maxW={"5xl"} minH={"calc(100vh - 60px)"} mt={"60px"} py={20}>
@@ -12,6 +16,18 @@ export default function About() {
           <Mission />
           <Qualities />
           <Faq />
+          <Button
+            mt="-12"
+            type="submit"
+            loadingText="Submitting..."
+            size="lg"
+            colorScheme="yellow"
+            leftIcon={<Icon as={FaUtensils} />}
+            variant={"outline"}
+            onClick={() => router.push("/products")}
+          >
+            Browse Now
+          </Button>
         </VStack>
       </Container>
     </main>
