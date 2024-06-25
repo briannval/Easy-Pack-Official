e6dd5c8f86fdcf767d483859d26c1c2c49a51a8d
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-06-25 09:00:53 +0700

    next-intl additional sitemap and layout setup

--- src/config.ts
+++ src/config.ts
@@ -12,3 +12,8 @@ export const pathnames: Pathnames<typeof locales> = {
 };
 
 export const localePrefix: LocalePrefix<typeof locales> = "always";
+
+export const port = process.env.PORT || 3000;
+export const host = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : `http://localhost:${port}`;
