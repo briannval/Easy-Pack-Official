ea69fd0449939d692942d681c04535b8298e1721
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-07-01 12:49:44 +0700

    Removed botpress from mobile (screen height error)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import Script from "next/script";
+import { BrowserView } from "react-device-detect";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -59,12 +60,13 @@ export default async function LocaleLayout({
           </ChakraProvider>
         </NextIntlClientProvider>
       </body>
-      <script
-        async
-        src="https://cdn.botpress.cloud/webchat/v1/inject.js"
-      ></script>
-      <Script async id="botpress-webchat" strategy="lazyOnload">
-        {`
+      <BrowserView>
+        <script
+          async
+          src="https://cdn.botpress.cloud/webchat/v1/inject.js"
+        ></script>
+        <Script async id="botpress-webchat" strategy="lazyOnload">
+          {`
           window.botpressWebChat.init({
             "composerPlaceholder": "Chat with Easy Bot",
             "botConversationDescription": "Official bot for Easy Pack",
@@ -84,7 +86,8 @@ export default async function LocaleLayout({
             "allowedOrigins": []
           });
         `}
-      </Script>
+        </Script>
+      </BrowserView>
     </html>
   );
 }
