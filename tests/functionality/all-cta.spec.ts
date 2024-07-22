9c8f584c13aea7d9f8ee99fe8bce75e8653b40d5
brianadhitya <brianvalentinoadhitya@gmail.com>
2024-07-22 17:00:06 +0700

    Completed CTA tests

--- tests/functionality/all-cta.spec.ts
+++ tests/functionality/all-cta.spec.ts
@@ -11,9 +11,17 @@ test.describe("CTA Buttons", () => {
 
   test("home about CTA", async ({ page }) => {
     await page.goto("/");
+
+    await page.click("a#view-about");
+
+    await expect(page).toHaveURL(/.*about/);
   });
 
   test("about products CTA", async ({ page }) => {
     await page.goto("/about");
+
+    await page.click("a#view-products");
+
+    await expect(page).toHaveURL(/.*products/);
   });
 });
